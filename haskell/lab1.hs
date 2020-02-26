7b407a954388553a800f868ccf24e9647aeb0428
Iga <iga.supinska@gmail.com>
2020-02-26 22:27:21 +0100

    before 2nd lab

--- haskell/lab1.hs
+++ haskell/lab1.hs
@@ -22,4 +22,26 @@ myHead :: [a]  -> a
 myHead (x:xs) = x
 
 myTail :: [a]  -> a
-myTail (xs++x) = x
+myTail [x] = x
+myTail (x:xs) = myTail xs
+
+-- myPlus :: [a] -> [a] -> [a]
+-- myPlus [] [] = []
+-- myPlus [] x = x
+-- myPlus x [] = x
+-- myPlus x (y:ys) = myPlus (x:y) ys
+
+myTake :: [a] -> Int -> [a]
+myTake [] i = []
+myTake x 0 = []
+myTake (x:xs) i = (x:myTake xs (i-1))
+
+
+myDrop :: [a] -> Int -> [a]
+myDrop [] i = []
+myDrop x 0 = x
+myDrop (x:xs) i = myDrop xs (i-1)
+
+myFilter :: (a->Bool) -> [a] -> [a]
+myFilter cond [] = []
+myFilter cond (x:xs) = if cond x then (x:myFilter cond xs) else myFilter cond xs
