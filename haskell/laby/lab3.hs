dc0c6b72c1e5c53cf0933ed16994a831d3c65697
Iga <iga.supinska@gmail.com>
2020-03-11 21:25:32 +0100

    dodane drzewa

--- haskell/laby/lab3.hs
+++ haskell/laby/lab3.hs
@@ -33,3 +33,42 @@ fromEither (Right a) = a
 reverseRight :: Either e [a] -> Either e [a]
 reverseRight (Left e) = Left e
 reverseRight (Right a) = Right (reverse a)
+
+-- trees
+
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Ord)
+
+instance Show a => Show (Tree a) where
+   show Empty = ""
+   show (Node a Empty Empty) = show a
+   show (Node a l r) = show l ++ " " ++ show a ++ " " ++ show r
+
+instance Eq a => Eq (Tree a) where
+    Empty == Empty = True
+    Empty == Node a l r = False
+    (Node a l r) == Empty = False
+    (Node a l r) == (Node b m n) = (a == b) && (l == m) && (r == n)
+
+toList :: Tree a -> [a]
+toList Empty = []
+toList (Node a Empty Empty) = [a]
+toList (Node a l r) = toList l ++ a:(toList r)
+
+-- bst tree
+
+insert :: (Ord a) => a -> Tree a -> Tree a
+insert x Empty = Node x Empty Empty
+insert x (Node a l r)
+    | x == a = Node x l r
+    | x < a  = Node a (insert x l) r
+    | x > a  = Node a l (insert x r) 
+
+contains :: (Ord a) => a -> Tree a -> Bool
+contains x Empty = False
+contains x (Node a l r)
+    | x == a = True
+    | x < a  = contains x l
+    | x > a  = contains x r
+
+fromList :: (Ord a) => [a] -> Tree a
+fromList x = foldr insert Empty x
