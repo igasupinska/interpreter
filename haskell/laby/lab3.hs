dc9be4a7667d1e7255550014303a6f593a014677
Iga <iga.supinska@gmail.com>
2020-03-16 10:32:42 +0100

    dodane wyrażenia

--- haskell/laby/lab3.hs
+++ haskell/laby/lab3.hs
@@ -72,3 +72,54 @@ contains x (Node a l r)
 
 fromList :: (Ord a) => [a] -> Tree a
 fromList x = foldr insert Empty x
+
+data Exp 
+  = EInt Int             -- stała całkowita       
+  | EAdd Exp Exp         -- e1 + e2
+  | ESub Exp Exp         -- e1 - e2
+  | EMul Exp Exp         -- e1 * e2
+  | EVar String          -- zmienna
+  | ELet String Exp Exp  -- let var = e1 in e2 
+
+instance Eq Exp where
+    (EInt x) == (EInt y) = x == y 
+
+instance Show Exp where
+  show (EInt x) = show x    
+  show (EAdd e1 e2) = show e1 ++ " + " ++ show e2
+  show (ESub e1 e2) = show e1  ++ " - " ++ show e2
+  show (EMul e1 e2) = show e1 ++ " * " ++ show e2
+  show (EVar x) = show x
+  show (ELet x e1 e2) = "let " ++ show x ++ " = " ++ show e1 ++ " in " ++ show e2 
+
+instance Num Exp where
+    (+) x y = EAdd x y 
+    (-) x y = ESub x y
+    (*) x y = EMul x y
+    negate x = ESub 0 x
+    abs x = undefined
+    signum x = undefined
+    fromInteger x = EInt (fromIntegral x)
+
+-- simplify Exp
+-- simpl (EAdd (EMul (EInt 0) (EVar "x")) (EMul (EInt 1) (EVar "y")))
+simpl :: Exp -> Exp
+simpl (EInt x) = EInt x
+simpl (EVar x) = EVar x
+simpl (EMul x y) = simplOnce (EMul (simpl x) (simpl y))
+simpl (EAdd x y) = simplOnce (EAdd (simpl x) (simpl y))
+
+simplOnce :: Exp -> Exp
+simplOnce (EMul (EInt 1) y) = simpl y
+simplOnce (EMul y (EInt 1)) = simpl y
+simplOnce (EMul (EInt 0) _) = EInt 0
+simplOnce (EMul _ (EInt 0)) = EInt 0
+simplOnce (EMul x y) = EMul x y
+simplOnce (EAdd (EInt 0) x) = simpl x
+simplOnce (EAdd x (EInt 0)) = simpl x
+simplOnce (EAdd x y) = EAdd x y
+
+testExp2 :: Exp
+testExp2 = (2 + 2) * 3
+
+
