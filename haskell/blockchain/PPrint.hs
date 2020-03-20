79b8eef005747a5e7450aa462c5b06f04590fb30
Iga <iga.supinska@gmail.com>
2020-03-20 17:20:49 +0100

    everything working; to do: add element to leaf + clean code

--- haskell/blockchain/PPrint.hs
+++ haskell/blockchain/PPrint.hs
@@ -3,27 +3,26 @@ module PPrint where
 writeln :: String -> IO ()
 writeln = putStrLn
 
--- Iga: ShowS = String -> String
 showsPair :: Show a => (String, a) -> ShowS
 showsPair (k,v) = ((k ++ ": " ++ show v) ++)
 
 pprH, pprV :: [ShowS] -> ShowS
-pprV l = intercalateS ("\n" ++) l
-pprH l = intercalateS ("_" ++) l
+pprV = intercalateS ("\n" ++)
+pprH = intercalateS (" " ++)
 
 intercalateS :: ShowS -> [ShowS] -> ShowS
-intercalateS sep l = ((intercalateSHelper sep l) ++)
+intercalateS sep (l:ls) = (l (intercalateSHelper sep ls) ++)
 
 intercalateSHelper :: ShowS -> [ShowS] -> String
-intercalateSHelper sep [l] = sep (l "")
+intercalateSHelper sep [] = []
 intercalateSHelper sep (l:ls) = sep (l (intercalateSHelper sep ls))
 
 pprListWith :: (a -> ShowS) -> [a] -> ShowS
-pprListWith f l = ((pprListWithHelper f l) ++)
+pprListWith f (l:ls) = ((f l) (pprListWithHelper f ls) ++)
 
 pprListWithHelper :: (a -> ShowS) -> [a] -> String
-pprListWithHelper f [] = ""
-pprListWithHelper f (l:ls) = f l (pprListWithHelper f ls)
+pprListWithHelper f [] = []
+pprListWithHelper f (l:ls) = "\n" ++ f l (pprListWithHelper f ls)
 
 runShows :: ShowS -> IO ()
 runShows = putStrLn . ($"")
