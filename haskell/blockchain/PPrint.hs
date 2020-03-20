a25ad5407abb8377ffb4068b9c263133f3599a68
Iga <iga.supinska@gmail.com>
2020-03-20 15:03:59 +0100

    rough version of printing, works ok except for invalid formatting

--- haskell/blockchain/PPrint.hs
+++ haskell/blockchain/PPrint.hs
@@ -3,18 +3,27 @@ module PPrint where
 writeln :: String -> IO ()
 writeln = putStrLn
 
+-- Iga: ShowS = String -> String
 showsPair :: Show a => (String, a) -> ShowS
-showsPair (k,v) = undefined
+showsPair (k,v) = ((k ++ ": " ++ show v) ++)
 
 pprH, pprV :: [ShowS] -> ShowS
-pprV = intercalateS undefined
-pprH = intercalateS undefined
+pprV l = intercalateS ("\n" ++) l
+pprH l = intercalateS ("_" ++) l
 
 intercalateS :: ShowS -> [ShowS] -> ShowS
-intercalateS sep list = undefined
+intercalateS sep l = ((intercalateSHelper sep l) ++)
+
+intercalateSHelper :: ShowS -> [ShowS] -> String
+intercalateSHelper sep [l] = sep (l "")
+intercalateSHelper sep (l:ls) = sep (l (intercalateSHelper sep ls))
 
 pprListWith :: (a -> ShowS) -> [a] -> ShowS
-pprListWith = undefined
+pprListWith f l = ((pprListWithHelper f l) ++)
+
+pprListWithHelper :: (a -> ShowS) -> [a] -> String
+pprListWithHelper f [] = ""
+pprListWithHelper f (l:ls) = f l (pprListWithHelper f ls)
 
 runShows :: ShowS -> IO ()
 runShows = putStrLn . ($"")
