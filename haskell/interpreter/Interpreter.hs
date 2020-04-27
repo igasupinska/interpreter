bc2f9e99d56e684478ee3dd867069a3820a9033b
Iga <iga.supinska@gmail.com>
2020-04-27 10:22:27 +0200

    basic version with just expressions

---
+++ haskell/interpreter/Interpreter.hs
@@ -0,0 +1,177 @@
+module Interpreter where
+    import AbsGramm
+
+    import Control.Monad.Reader
+    import Control.Monad.State.Lazy
+    import Data.Map as Map
+    import Data.Maybe
+    import Prelude hiding (lookup)
+    
+    data StoredVal = SInt Integer | SStr String | SBool Bool -- Iga: co z tablicami? Iga: czy to nie widzne, ze piszesz ze soba?
+
+    type Loc = Integer
+    type Store = Map Loc StoredVal
+    type Env = Map Ident Loc
+
+    lookupVar :: Ident -> Env -> Loc
+    lookupVar name env = maybe 0 id (lookup name env)
+
+    lookupStore :: Loc -> Store -> StoredVal
+    lookupStore loc store = fromMaybe (SInt 0) (lookup loc store)
+
+--------------------------------------------------
+----------------- EXPRESSIONS --------------------
+--------------------------------------------------
+
+    evalExpr :: Expr -> MM (StoredVal)
+
+    -- evalExpr (EVar ident) = do
+    --     loc <- asks (lookupVar ident)
+    --     val <- 
+    --     return $ (SInt loc)
+    
+    --int expr
+    evalExpr (ELitInt x) = return $ SInt x
+    
+    evalExpr (Neg e) = do
+        SInt i <- evalExpr e
+        return $ SInt (-i)
+    
+    evalExpr (EMul e1 Times e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        return $ SInt $ i1 * i2
+
+    evalExpr (EMul e1 Div e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        if i2 == 0
+            then return $ SStr "Division by zero!" --Iga: tu poprawić
+            else return $ SInt $ i1 `div` i2 
+
+    evalExpr (EMul e1 Mod e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        if i2 == 0
+            then return $ SStr "Division by zero!" --Iga: tu poprawić
+            else return $ SInt $ i1 `mod` i2 
+    
+    evalExpr (EAdd e1 Plus e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        return $ SInt $ i1 + i2
+    
+    evalExpr (EAdd e1 Minus e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        return $ SInt $ i1 - i2
+
+    --string expr
+    evalExpr (EString s) = return $ SStr s
+    
+    -- --bool expr
+    evalExpr (ELitTrue) = return $ SBool True
+    
+    evalExpr (ELitFalse) = return $ SBool False
+    
+    evalExpr (Not e) = do
+        SBool expr <- evalExpr e
+        return $ SBool $ not expr
+    
+    evalExpr (ERel e1 op e2) = do
+        SInt i1 <- evalExpr e1
+        SInt i2 <- evalExpr e2
+        case op of
+            LTH -> return $ SBool $ i1 < i2 
+            LE  -> return $ SBool $ i1 <= i2 
+            GTH -> return $ SBool $ i1 > i2 
+            GE  -> return $ SBool $ i1 >= i2 
+            EQU -> return $ SBool $ i1 == i2 
+            NE  -> return $ SBool $ i1 /= i2 
+
+    evalExpr (EAnd e1 e2) = do
+        SBool b1 <- evalExpr e1
+        SBool b2 <- evalExpr e2
+        return $ SBool $ b1 && b2
+    
+    evalExpr (EOr e1 e2) = do
+        SBool b1 <- evalExpr e1
+        case b1 of
+            True -> return $ SBool True
+            False -> evalExpr e2
+
+    -- --function expr
+    -- evalExpr (EApp fun (ERefArg a:as)) = undefined
+    -- evalExpr (EApp fun (EExpArg a:as)) = undefined
+
+    -- --array expr
+    -- evalExprHelper (ArrAcc a e) = undefined
+
+
+--------------------------------------------------
+------------------ STATEMENTS --------------------
+--------------------------------------------------
+
+    execStmt :: Stmt -> MM (Maybe StoredVal)
+    execStmt (BStmt b) = undefined
+    execStmt (Decl t e) = undefined
+    execStmt (Ass v e) = undefined
+    
+    execStmt (Ret e) = do
+        expr <- evalExpr e
+        return $ Just expr
+
+    execStmt (VRet) = return Nothing
+    
+    execStmt (Cond e b) = do
+        expr <- evalExpr e
+        case expr of
+            SBool True -> execStmt (BStmt b)
+            SBool False -> execStmt VRet --tymczasowo
+    
+    execStmt (CondElse e if_b else_b) = do
+        expr <- evalExpr e
+        case expr of
+            SBool True  -> execStmt $ BStmt if_b
+            SBool False -> execStmt $ BStmt else_b
+
+    execStmt (While e b) = undefined
+    execStmt (For v start end b) = undefined
+    
+    execStmt (Print e) = do
+        expr <- evalExpr e
+        case expr of
+            SInt expr  -> do
+                        -- liftIO $ putStrLn "Print int"
+                        execStmt VRet --Iga: tymczasowo
+            SBool expr ->
+                        -- liftIO $ putStrLn "Print bool"
+                        execStmt VRet --Iga: tymczasowo
+            SStr expr  ->
+                        -- liftIO $ putStrLn "print string"
+                        execStmt VRet --Iga: tymczasowo
+    
+    execStmt (SExp e) = undefined
+    execStmt (Break) = undefined
+    execStmt (Cont) = undefined
+
+
+--------------------------------------------------
+--------------------- RUN ------------------------
+--------------------------------------------------
+
+    interpretFun :: Program -> MM (Maybe StoredVal)
+    interpretFun (Program [FnDef Int f as (Block s)]) = interpret s
+    interpretFun (Program (d:ds)) = interpretFun (Program [d])
+
+    interpret :: [Stmt] -> MM (Maybe StoredVal)
+    interpret [] = undefined
+    interpret (s:xs) = execStmt s
+    
+    runProg prog = runState (runReaderT (interpretFun prog) Map.empty) (Map.empty) --Iga: skopiowane
+
+    -- evalExpr :: Expr -> Store -> StoredVal
+    -- evalExpr x s = runReader (evalExprHelper x) s
+
+    --my monad
+    type MM = ReaderT Env (State Store)
---
+++ haskell/interpreter/Main.hs
@@ -0,0 +1,63 @@
+import AbsGramm
+import LexGramm
+import ParGramm
+import ErrM
+
+import Interpreter
+
+import System.IO (stdin, hGetContents, hPutStrLn, stderr, getContents, hPutStr)
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure, exitSuccess)
+import Control.Exception (catch, IOException)
+
+printResult :: (Maybe StoredVal, Store) -> String
+printResult (Just (SInt res), store) = show res 
+printResult (Just (SBool res), store) = show res 
+printResult (Just (SStr res), store) = show res
+printResult (Nothing, store) = "Void prog"
+
+main :: IO () --Iga: skopiowane
+main = do
+    args <- getArgs
+    case args of
+        [] -> getContents >>= parse
+        files -> mapM_ parseFile files
+
+parseFile :: String -> IO () --Iga: skopiowane
+parseFile fileName = readFile fileName >>= parse
+
+parse :: String -> IO () --Iga: skopiowane
+parse input = let src = myLexer input in
+        case pProgram src of
+            Bad c ->  do
+                        putStrLn "Parse Failed...\n"
+                        exitFailure
+            Ok c  ->  do
+                        putStrLn "Parse Successful!\n"
+                        putStrLn $ printResult $ runProg c
+                        exitSuccess
+
+
+-- main :: IO ()
+-- main = do
+--   args <- getArgs
+--   case args of
+--     ["--help"] -> usage
+--     [] -> hGetContents stdin >>= run 2 pProgram
+--     "-s":fs -> mapM_ (runFile 0 pProgram) fs
+--     fs -> mapM_ (runFile 2 pProgram) fs
+
+-- runFile :: (Print a, Show a) => Verbosity -> ParseFun a -> FilePath -> IO ()
+-- runFile v p f = putStrLn f >> readFile f >>= run v p
+
+-- run :: (Print a, Show a) => Verbosity -> ParseFun a -> String -> IO ()
+-- run v p s = let ts = myLLexer s in case p ts of
+--            Bad s    -> do putStrLn "\nParse              Failed...\n"
+--                           putStrV v "Tokens:"
+--                           putStrV v $ show ts
+--                           putStrLn s
+--                           exitFailure
+--            Ok  tree -> do putStrLn "\nParse Successful!"
+--                           showTree v tree
+
+--                           exitSuccess
