ebf034fa1006031734ffc1dcc2350157ac6d3a93
Iga <iga.supinska@gmail.com>
2020-05-04 19:51:23 +0200

    errors cleanup

--- haskell/interpreter/TypeChecker.hs
+++ haskell/interpreter/TypeChecker.hs
@@ -33,7 +33,7 @@ module TypeChecker where
 
 
     lookupVar :: Ident -> [VEnvT] -> TM (Type)
-    lookupVar ident [] = throwError ("Variable " ++ show ident ++ " not declared")
+    lookupVar (Ident ident) [] = throwError ("Variable " ++ ident ++ " not declared")
 
     lookupVar ident (e:es) =
         if member ident e
@@ -56,6 +56,19 @@ module TypeChecker where
 ----------------- EXPRESSIONS --------------------
 --------------------------------------------------
 
+    correctTypes :: Type -> [Expr] -> TM ()
+    correctTypes expT (e:es) = do
+        correctType expT e
+        correctTypes expT es
+        return ()
+
+    correctType :: Type -> Expr -> TM ()
+    correctType expT e = do
+        t <- checkExpr e
+        if t == expT
+            then return ()
+            else throwError ("Expected " ++ show expT ++ " but got " ++ show t)
+
     checkExpr :: Expr -> TM (Type)
 
     checkExpr (EVar ident) = do
@@ -65,26 +78,21 @@ module TypeChecker where
     checkExpr (ELitInt _) = return Int
     
     checkExpr (Neg e) = do
-        t <- checkExpr e
-        if t == Int
-            then return Int
-            else throwError ("Negating type " ++ show t)
+        correctType Int e
+        return Int
+        `catchError` \err -> throwError ("Type error: in " ++ show (Neg e)  ++ " operation. "++ err)
 
     checkExpr (EMul e1 op e2) = do
-        t1 <- checkExpr e1
-        t2 <- checkExpr e2
-        if (t1 == Int && t2 == Int)
-            then return Int
-            else
-                throwError ("Trying to do multiply/divide/modulo on " ++ (show t1) ++ "and " ++ (show t2))
+        correctType Int e1
+        correctType Int e2
+        return Int
+        `catchError` \err -> throwError ("Type error: in " ++ show op  ++ " operation. "++ err)
 
     checkExpr (EAdd e1 op e2) = do
-        t1 <- checkExpr e1
-        t2 <- checkExpr e2
-        if (t1 == Int && t2 == Int)
-            then return Int
-            else
-                throwError ("Trying to do add/subtract on " ++ (show t1) ++ "and " ++ (show t2))
+        correctType Int e1
+        correctType Int e2
+        return Int
+        `catchError` \err -> throwError ("Type error: in" ++ (show op) ++ "operation. " ++ err)
 
     -- --string expr
     checkExpr (EString _) = return Str
@@ -95,46 +103,41 @@ module TypeChecker where
     checkExpr (ELitFalse) = return Bool
     
     checkExpr (Not e) = do
-        t <- checkExpr e
-        if t == Bool
-            then return Bool
-            else throwError ("Type error: !" ++ show t)
+        correctType Bool e
+        return Bool
+        `catchError` \err -> throwError ("Type error: in " ++ show (Not e) ++ " operation. " ++ err)
     
     checkExpr (ERel e1 op e2) = do
-        t1 <- checkExpr e1
-        t2 <- checkExpr e2
-        if (t1 == Int && t2 == Int)
-            then return Bool
-            else throwError ("Type error: rel operation on " ++ show t1 ++ " and " ++ show t2)
+        correctType Int e1
+        correctType Int e2
+        return Bool
+        `catchError` \err -> throwError ("Type error: in" ++ (show op) ++ "operation. " ++ err)
 
     checkExpr (EAnd e1 e2) = do
-        t1 <- checkExpr e1
-        t2 <- checkExpr e2
-        if (t1 == Bool && t2 == Bool)
-            then return Bool
-            else throwError ("Type error: " ++ show t1 ++ " && " ++ show t2)
-    
+        correctType Bool e1
+        correctType Bool e2
+        return Bool
+        `catchError` \err -> throwError ("Type error: in && operation. " ++ err)
+
     checkExpr (EOr e1 e2) = do
-        t1 <- checkExpr e1
-        t2 <- checkExpr e2
-        if (t1 == Bool && t2 == Bool)
-            then return Bool
-            else throwError ("Type error: " ++ show t1 ++ " || " ++ show t2)
+        correctType Bool e1
+        correctType Bool e2
+        return Bool
+        `catchError` \err -> throwError ("Type error: in || operation. " ++ err)
 
     --array expr
     checkExpr (ArrAcc a e) = do
-        t <- checkExpr e
-        if t /= Int
-            then throwError ("Type error: trying to access array at index of type " ++ show t)
-            else do
-                (venv, fenv) <- ask
-                t <- lookupVar a venv
-                case t of
-                    Arr x -> return x
-                    _ -> throwError ("Type error: not an array but " ++ show t)
+        correctType Int e
+        (venv, fenv) <- ask
+        t <- lookupVar a venv
+        case t of
+            Arr x -> return x
+            _     -> throwError ("Not an array but " ++ show t)
+        `catchError` \err -> throwError ("Type error: in array access. " ++ err)
 
     -- --function expr
 
+    --Iga:upiekszyc
     --Iga: type FEnvT = Map Ident ([ArgType], [VEnvT])
     checkExpr (EApp ident rArgs) = do
         (venv, fenv) <- ask
@@ -142,6 +145,7 @@ module TypeChecker where
         validateArgs rArgs fArgs
         return ret
 
+    --Iga:upiekszyc
     --check if formal argument type is the same as actual one
     validateArgs :: [ExprOrRef] -> [ArgType] -> TM ()
     validateArgs [] [] = return ()
@@ -155,7 +159,7 @@ module TypeChecker where
                 if t /= t'
                     then throwError ("Argument types don't match")
                     else validateArgs rs fs
-
+    --Iga:upiekszyc
     validateArgs (EExpArg r:rs) (f:fs) = do
         case f of
             (ident, t, True) -> throwError("Expected reference but got expression argument")
@@ -170,13 +174,15 @@ module TypeChecker where
 ------------------ STATEMENTS --------------------
 --------------------------------------------------
 
+    --Iga:upiekszyc
     checkRedecl :: Ident -> TM ()
     checkRedecl ident = do
         ((venv:venvs), _) <- ask
         if member ident venv
-            then throwError ("Variable " ++ show ident ++ " already declared")
+            then throwError ("Variable " ++ show ident ++ " already declared.")
             else return ()
-
+    
+    --Iga:upiekszyc
     checkStmtBlockHelper :: Block -> TM ()
     checkStmtBlockHelper (Block []) = return ()
     checkStmtBlockHelper (Block (b:bs)) = do
@@ -199,56 +205,42 @@ module TypeChecker where
         return (venv', fenv)
 
     checkStmt (Decl t (Init ident expr)) = do
-        tExpr <- checkExpr expr
-        if tExpr == t
-            then checkStmt (Decl t (NoInit ident))
-            else throwError ("Type error: initializing variable of type " ++ show t ++ " with expression of type " ++ show tExpr)
+        correctType t expr
+        checkStmt (Decl t (NoInit ident))
+        `catchError` \err -> throwError ("Type error: in initializing variable. " ++ err)
 
     checkStmt (Decl (Arr t) (ArrNoInit ident expr)) = do
         checkRedecl ident
-        t1 <- checkExpr expr
-        if t1 /= Int
-            then throwError ("Type error: cannot declare array of size " ++ show t1)
-            else do
-                (venv, env) <- ask
-                let venv' = insertVar ident (Arr t) venv
-                return (venv', env)
+        correctType Int expr
+        (venv, fenv) <- ask
+        let venv' = insertVar ident (Arr t) venv
+        return (venv', fenv)
+        `catchError` \err -> throwError ("Type error: in array size expression. " ++ err)
     
-    checkStmt (Decl (Arr t) (ArrInit ident expr [])) = do
+    checkStmt (Decl (Arr t) (ArrInit ident expr l)) = do
         env <- checkStmt (Decl (Arr t) (ArrNoInit ident expr))
+        correctTypes t l
         return env
+        `catchError` \err -> throwError ("Type error: list initialization. " ++ err)
 
-
-    --Iga: check that l.length is the size of array
-    checkStmt (Decl (Arr t) (ArrInit ident expr (l:ls))) = do
-        t1 <- checkExpr l
-        if t /= t1
-            then throwError ("Type error: initialization list contains " ++ show t1 ++ " instead of " ++ show t)
-            else checkStmt (Decl (Arr t) (ArrInit ident expr ls))
 
     checkStmt (Ass ident e) = do
         (venv, fenv) <- ask
         t1 <- lookupVar ident venv
-        t2 <- checkExpr e
-        if t1 == t2
-            then return (venv, fenv)
-            else throwError ("Type error: assigning expression of type " ++ show t2 ++ " to variable of type " ++ show t2)
+        correctType t1 e
+        return (venv, fenv)
+        `catchError` \err -> throwError ("Type error: in assignment. " ++ err)
     
-    checkStmt (ArrAss ident idx_e e) = do
-        t <- checkExpr idx_e
-        if t /= Int
-            then throwError ("Type error: array index should be Int, not " ++ show t)
-            else do
-                (venv, fenv) <- ask
-                t1 <- lookupVar ident venv
-                case t1 of
-                    Arr x -> do
-                        t <- checkExpr e
-                        if x == t
-                            then return (venv, fenv)
-                            else throwError ("Type error: trying to assign " ++ show t ++ " to array of type " ++ show x)
-                    _ -> throwError ("Type error: trying to assing to array, but it's " ++ show t1)
-
+    checkStmt (ArrAss id@(Ident ident) idx_e e) = do
+        correctType Int idx_e
+        (venv, fenv) <- ask
+        t1 <- lookupVar id venv
+        case t1 of
+            Arr x -> do
+                correctType x e
+                return (venv, fenv)
+            _ -> throwError (ident ++ " is not an array.")
+        `catchError` \err -> throwError ("Type error: array assignment. " ++ err)
 
     checkStmt (Ret e) = do
         t1 <- checkExpr e
@@ -261,38 +253,36 @@ module TypeChecker where
         env <- ask
         return env
     
-    checkStmt (Cond e _ ) = do
+    checkStmt (Cond e _) = do
         env <- ask
-        t1 <- checkExpr e
-        if t1 == Bool
-            then return env
-            else throwError ("While condition of type " ++ show t1)
+        correctType Bool e
+        return env
+        `catchError` \err -> throwError ("Type error: if condition. " ++ err)
 
     checkStmt (CondElse e _ _) = do
         env <- ask
-        t1 <- checkExpr e
-        if t1 == Bool
-            then return env
-            else throwError ("While condition of type " ++ show t1)
+        correctType Bool e
+        return env
+        `catchError` \err -> throwError ("Type error: if-else condition. " ++ err)
 
     checkStmt (While e _) = do
         env <- ask
-        t1 <- checkExpr e
-        if t1 == Bool
-            then return env
-            else throwError ("While condition of type " ++ show t1)
-
+        correctType Bool e
+        return env
+        `catchError` \err -> throwError ("Type error: while condition. " ++ err)
 
     checkStmt (For v start end (Block b)) = do
         env <- ask
         --Iga:sprawdzić, że zmienna v jest typu int
-        t1 <- checkExpr start
-        t2 <- checkExpr end
-        if (t1 == Int && t2 == Int)
-            then return env
-            else throwError ("For loop from " ++ show t1 ++ " to " ++ show t2)
+        correctType Int start
+        correctType Int end
+        correctType Int (EVar v)
+        return env
+        `catchError` \err -> throwError ("Type error: for loop. " ++ err)
         --Iga:co z blokiem?
 
+
+    --Iga: co z tym poniżej
     checkStmt (Print e) = do
         env <- ask
         t <- checkExpr e
